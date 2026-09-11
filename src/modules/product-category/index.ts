333d5f65aa92e05a2731b0e18be828ed03c3d649
zuhalcode <zuhalcode@gmail.com>
2026-09-11 11:35:42 +0700

    refactor(product): remove product category module and related files

--- src/modules/product-category/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as productCategoryController } from "./product-category.controller";
-export { default as productCategoryService } from "./product-category.service";
--- src/modules/product/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as productController } from "./product.controller";
-export { default as productService } from "./product.service";
