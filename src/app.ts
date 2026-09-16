b0f86096db85a5793bdbbc4ea8d572ca4acd9aee
zuhalcode <zuhalcode@gmail.com>
2026-09-16 13:01:24 +0700

    refactor(app): enhance Swagger UI setup by adding explicit routes for static assets

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import apiRouter from "./routes";
 import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import swaggerUiDist from "swagger-ui-dist";
+import path from "node:path";
+import fs from "node:fs";
 
 import { corsMiddleware } from "./configs/cors";
 import { swaggerDocument } from "./docs/swagger";
@@ -10,10 +12,47 @@ import { swaggerDocument } from "./docs/swagger";
 const app = express();
 
 // Swagger UI static assets
-app.use("/swagger-ui", express.static(swaggerUiDist.getAbsoluteFSPath()));
+const swaggerUiPath = swaggerUiDist.getAbsoluteFSPath();
+
+app.use("/swagger-ui", express.static(swaggerUiPath));
+
+app.get("/swagger-ui/swagger-ui.css", (_, res) => {
+  res
+    .type("text/css")
+    .send(fs.readFileSync(path.join(swaggerUiPath, "swagger-ui.css"), "utf8"));
+});
+
+app.get("/swagger-ui/swagger-ui-bundle.js", (_, res) => {
+  res
+    .type("application/javascript")
+    .send(
+      fs.readFileSync(path.join(swaggerUiPath, "swagger-ui-bundle.js"), "utf8"),
+    );
+});
+
+app.get("/swagger-ui/swagger-ui-standalone-preset.js", (_, res) => {
+  res
+    .type("application/javascript")
+    .send(
+      fs.readFileSync(
+        path.join(swaggerUiPath, "swagger-ui-standalone-preset.js"),
+        "utf8",
+      ),
+    );
+});
 
 // Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCssUrl: "/swagger-ui/swagger-ui.css",
+    customJs: [
+      "/swagger-ui/swagger-ui-bundle.js",
+      "/swagger-ui/swagger-ui-standalone-preset.js",
+    ],
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
