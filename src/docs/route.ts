f05ce473cde70a9bf52772dd35eb4e3161991648
zuhalcode <zuhalcode@gmail.com>
2026-09-16 13:27:01 +0700

    refactor(app): update Swagger UI setup to use custom CSS and change route to /api-docs

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,10 +1,24 @@
-import express from "express";
-import type { Express } from "express";
-
+import { Express } from "express";
 import swaggerUi from "swagger-ui-express";
+
+import fs from "fs";
+import path from "path";
 import { swaggerDocument } from "./swagger";
-import { swaggerUiOptions, swaggerUiPath } from "./swagger-ui";
 
 export default function docs(app: Express) {
-  app.use("/swagger-ui", express.static(swaggerUiPath));
+  const css = fs.readFileSync(
+    path.resolve(
+      __dirname,
+      "../../node_modules/swagger-ui-dist/swagger-ui.css",
+    ),
+    "utf-8",
+  );
+
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+      customCss: css,
+    }),
+  );
 }
