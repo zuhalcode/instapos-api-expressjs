3819a3bc148f36eb05db99ff5e5b02ea2d6b021e
zuhalcode <zuhalcode@gmail.com>
2026-09-16 12:58:37 +0700

    refactor(app): simplify Swagger UI setup by using express.static for static assets

--- src/app.ts
+++ src/app.ts
@@ -3,42 +3,16 @@ import apiRouter from "./routes";
 import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import swaggerUiDist from "swagger-ui-dist";
-import path from "node:path";
-import fs from "node:fs";
 
 import { corsMiddleware } from "./configs/cors";
 import { swaggerDocument } from "./docs/swagger";
 
 const app = express();
 
+// Swagger UI static assets
+app.use("/swagger-ui", express.static(swaggerUiDist.getAbsoluteFSPath()));
+
 // Swagger
-const swaggerUiPath = swaggerUiDist.getAbsoluteFSPath();
-
-app.get("/swagger-ui/swagger-ui.css", (req, res) => {
-  res
-    .type("text/css")
-    .send(fs.readFileSync(path.join(swaggerUiPath, "swagger-ui.css"), "utf8"));
-});
-
-app.get("/swagger-ui/swagger-ui-bundle.js", (req, res) => {
-  res
-    .type("application/javascript")
-    .send(
-      fs.readFileSync(path.join(swaggerUiPath, "swagger-ui-bundle.js"), "utf8"),
-    );
-});
-
-app.get("/swagger-ui/swagger-ui-standalone-preset.js", (req, res) => {
-  res
-    .type("application/javascript")
-    .send(
-      fs.readFileSync(
-        path.join(swaggerUiPath, "swagger-ui-standalone-preset.js"),
-        "utf8",
-      ),
-    );
-});
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
