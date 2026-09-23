854dda0c6491cad5dc468f8e57390c939945a36d
zuhalcode <zuhalcode@gmail.com>
2026-09-23 09:35:25 +0700

    feat(database): update user role default value and enhance product creation function with initial stock setup

--- supabase/migrations/20260914004044_create_users.sql
+++ supabase/migrations/20260914004044_create_users.sql
@@ -11,9 +11,9 @@ create table public.users (
   id uuid primary key references auth.users(id) on delete cascade,
   name text not null,
   email text not null,
-  role public.user_role not null default 'customer',
+  role public.user_role not null default 'user',
   created_at timestamptz not null default now(),
-  updated_at timestamptz not null default now()
+  updated_at timestamptz default now()
 );
 
 create or replace function public.create_user_profile()
@@ -26,7 +26,8 @@ begin
   insert into public.users (
     id,
     name,
-    email
+    email,
+    role
   )
   values (
     new.id,
@@ -34,9 +35,12 @@ begin
       new.raw_user_meta_data->>'name',
       split_part(coalesce(new.email, ''), '@', 1)
     ),
-    new.email
+    new.email,
+    coalesce(
+      new.raw_user_meta_data->>'role',
+      'user'
+    )::public.user_role
   );
-
   return new;
 end;
 $$;
--- supabase/migrations/20260918040351_create_products.sql
+++ supabase/migrations/20260918040351_create_products.sql
@@ -6,7 +6,7 @@ create table public.products (
     price numeric(15, 2) not null,
     is_active boolean not null default true,
     created_at timestamptz not null default now (),
-    updated_at timestamptz null
+    updated_at timestamptz 
 );
 
 create table public.product_stocks (
@@ -15,3 +15,40 @@ create table public.product_stocks (
     location public.product_location not null,
     primary key (product_id, location)
 );
+
+-- Create product + initial stocks
+create or replace function public.create_product (
+    p_name varchar(150),
+    p_price numeric(15, 2)
+)
+
+returns public.products
+language plpgsql
+security invoker
+set search_path = public
+as $$
+declare
+    v_product public.products;
+begin
+    insert into public.products (
+        name,
+        price
+    )
+    values (
+        p_name,
+        p_price
+    )
+    returning * into v_product;
+
+    insert into public.product_stocks (
+        product_id,
+        location,
+        quantity
+    )
+    values
+        (v_product.id, 'warehouse', 0),
+        (v_product.id, 'display', 0);
+
+    return v_product;
+end;
+$$;
