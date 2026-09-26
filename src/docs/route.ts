c8514811cd9f430d5e8aa4ac3c726439eeac14f7
zuhalcode <zuhalcode@gmail.com>
2026-09-26 16:52:47 +0700

    refactor(docs): simplify CSS loading in API documentation setup

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -6,19 +6,13 @@ import path from "path";
 import { swaggerDocument } from "./swagger";
 
 export default function docs(app: Express) {
-  const css =
-    fs.readFileSync(
-      path.resolve(
-        __dirname,
-        "../../node_modules/swagger-ui-dist/swagger-ui.css",
-      ),
-      "utf-8",
-    ) +
-    `
-  .models.is-open {
-    display: none;
-  }
-`;
+  const css = fs.readFileSync(
+    path.resolve(
+      __dirname,
+      "../../node_modules/swagger-ui-dist/swagger-ui.css",
+    ),
+    "utf-8",
+  );
 
   app.use(
     "/api-docs",
