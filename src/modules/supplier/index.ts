962b061c8233c1a4f726e9f0968b985c69a0eefa
zuhalcode <zuhalcode@gmail.com>
2026-09-26 22:04:53 +0700

    feat(api): export SupplierRow type from supplier.types

--- src/modules/supplier/index.ts
+++ src/modules/supplier/index.ts
@@ -1,2 +1,3 @@
 export { default as supplierController } from "./supplier.controller";
 export { default as supplierService } from "./supplier.service";
+export { type SupplierRow } from "./supplier.types";
