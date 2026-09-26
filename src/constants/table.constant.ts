3ddd299575db74879f758dad766b26bb78c56de9
zuhalcode <zuhalcode@gmail.com>
2026-09-26 08:47:10 +0700

    feat(constants): remove table constants

--- src/constants/table.constant.ts
+++
@@ -1,8 +0,0 @@
-export const TABLES = {
-  USERS: "users",
-  PRODUCTS: "products",
-  CATEGORIES: "categories",
-  SALES: "sales",
-  SALE_ITEMS: "sale_items",
-  PAYMENTS: "payments",
-} as const;
